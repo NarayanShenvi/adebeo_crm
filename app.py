b6ef783758adf22d1a9a3e9b2a98335ebedab0d7
Narayan <narayan@adebeo.co.in>
2025-01-24 16:11:12 +0530

    app.py

--- app.py
+++ app.py
@@ -123,7 +123,8 @@ def login():
     if not user or not bcrypt.check_password_hash(user['password'], password):
         return jsonify({"error": "Invalid credentials"}), 401
 
-    access_token = create_access_token(identity={"username": username, "role": user['role']}, expires_delta=timedelta(hours=10))
+    #access_token = create_access_token(identity={"username": username, "role": user['role']}, expires_delta=timedelta(hours=10))
+    access_token = create_access_token(identity=username, expires_delta=timedelta(hours=10))
     return jsonify({"access_token": access_token,"role":user['role']}), 200
 
 
